d1d403fa46c2cc0945f90f5f5bd448cfaf4bb71b
mthompson11 <mthompson@Michaels-MacBook-Pro.local>
2022-03-21 21:26:16 -0700

    adding show functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,10 +1,26 @@
+const methodOverride = require('method-override');
 const Flight = require('../models/flight');
 
 function index(req, res){
     Flight.find({}, function(err, flights){
+        flights.sort(function(a,b){
+            if(a.departs > b.departs){
+                return 1
+            }
+            if(a.departs < b.departs){
+                return -1
+            }
+            return 0
+        })
         res.render('flights/index', {title: 'All Flights', flights});
     });
 };
+
+function show(req, res){
+    Flight.findById(req.params.id, function(err, flight){
+        res.render('flights/show', {title:'Flight Detail', flight});
+    })
+}
 
 function newFlight (req, res){
     const newFlight = new Flight();
@@ -18,12 +34,13 @@ function create (req,res){
     const flight = new Flight(req.body);
     flight.save(function(err){
         if(err) return res.redirect('/flights/new');
-        res.redirect('/flights/index');
+        res.redirect('/flights/');
     });
 };
 
 module.exports = {
     index,
+    show,
     new: newFlight,
     create
 }
--- models/flight.js
+++ models/flight.js
@@ -3,12 +3,17 @@ const Schema = mongoose.Schema;
 
 const today = new Date();
 
+const destinationSchema = new Schema({
+    airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
+    arrival: {type: Date}
+})
+
 const flightSchema = new Schema({
     airline: {type: String, enum: ['American', 'Southwest', 'United']},
     airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
     flightNo: {type: Number, min: 10, max: 9999},
-    departs: {type: Date, default: today.setFullYear(today.getFullYear() + 1)}
-
+    departs: {type: Date, default: today.setFullYear(today.getFullYear() + 1)},
+    destinations : [destinationSchema]
 }, {
     timestamps: true
 })
